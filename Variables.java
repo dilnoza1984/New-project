3ae971bd0998ece97e52694f3237a3bec72b57eb
DuotechB9 <duotechb9@gmail.com>
2022-09-22 19:50:20 -0400

    first commit

---
+++ Variables.java
@@ -0,0 +1,19 @@
+package scannerDemo;
+
+public class Variables {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+        int number = 56;  // variables must be initialized before using
+
+//        int anotherNum = 4 + number;
+
+        System.out.println(number);
+
+    }
+}
